5fcd0cef46ddb67461155fa0c40ab09ab2340363
Andrew Folga <andrew.folga@equalexperts.com>
2025-01-31 17:29:42 +0000

    Build a basic scoring page

--- src/server/controllers/mock-score-response.js
+++ src/server/controllers/mock-score-response.js
@@ -1,283 +1,119 @@
 export default {
-  grantScheme: {
-    key: 'LAYINGHENS01',
-    name: 'Laying Hens Grant'
-  },
-  desirability: {
-    questions: [
-      {
-        key: 'poultry-type',
-        answers: [
-          {
-            key: 'poultry-type',
-            title: 'What type of poultry will be housed in this building?',
-            input: [
-              {
-                key: 'poultry-type-A1',
-                value: 'Hens'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 0,
-          band: 'weak',
-          importance: null
+  questions: [
+    {
+      questionId: '/products-processed',
+      category: 'Produce processed',
+      fundingPriorities: [
+        'Create and expand processing capacity to provide more English-grown food for consumers to buy'
+      ],
+      answers: [
+        {
+          key: 'products-processed-A1',
+          value: 'Arable produce'
         }
-      },
-      {
-        key: 'current-system',
-        answers: [
-          {
-            key: 'current-system',
-            title:
-              'What type of hen housing system do you currently use in the building?',
-            input: [
-              {
-                key: 'current-system-A3',
-                value: 'Barn'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 7,
-          band: 'Average',
-          importance: null
-        }
-      },
-      {
-        key: 'ramp-connection',
-        answers: [
-          {
-            key: 'ramp-connection',
-            title:
-              'Will every level of the multi-tier system be connected to another level by a ramp?',
-            input: [
-              {
-                key: 'ramp-connection-A2',
-                value: 'No'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 0,
-          band: 'Weak',
-          importance: null
-        }
-      },
-      {
-        key: 'maximum-tier-height',
-        answers: [
-          {
-            key: 'maximum-tier-height',
-            title:
-              'Will the highest tier with direct access to the floor be 2 metres high or less?',
-            input: [
-              {
-                key: 'maximum-tier-height-A1',
-                value: 'Yes'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 5,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'tier-number',
-        answers: [
-          {
-            key: 'tier-number',
-            title:
-              'How many tiers will be positioned directly above each other in the {{_poultryType_}} system?',
-            input: [
-              {
-                key: 'tier-number-A1',
-                value: '3 tiers or fewer'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 5,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'hen-multi-tier',
-        answers: [
-          {
-            key: 'hen-multi-tier',
-            title:
-              'Will the hens in your grant-funded building be reared in a multi-tier system as pullets?',
-            input: [
-              {
-                key: 'hen-multi-tier-A1',
-                value: 'Yes'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 10,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'natural-light',
-        answers: [
-          {
-            key: 'natural-light',
-            title:
-              'Will the building have windows that provide natural light to the indoor housing?',
-            input: [
-              {
-                key: 'natural-light-A2',
-                value: 'No'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 0,
-          band: 'Weak',
-          importance: null
-        }
-      },
-      {
-        key: 'easy-grip-perches',
-        answers: [
-          {
-            key: 'easy-grip-perches',
-            title:
-              'Will the perches have a design feature that help the hens grip the perches?',
-            input: [
-              {
-                key: 'easy-grip-perches-A1',
-                value: 'Yes'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 15,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'building-biosecurity',
-        answers: [
-          {
-            key: 'building-biosecurity',
-            title: 'Will the building structure include the following?',
-            input: [
-              {
-                key: 'building-biosecurity-A1',
-                value: 'Shower facilities in the biosecurity changing area'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 7,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'pollution-mitigation',
-        answers: [
-          {
-            key: 'pollution-mitigation',
-            title: 'Will the building have any of the following?',
-            input: [
-              {
-                key: 'pollution-mitigation-A1',
-                value: 'Manure drying'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 7,
-          band: 'Strong',
-          importance: null
-        }
-      },
-      {
-        key: 'renewable-energy',
-        answers: [
-          {
-            key: 'renewable-energy',
-            title: 'Will the hen housing use renewable energy sources?',
-            input: [
-              {
-                key: 'renewable-energy-A1',
-                value: 'Solar PV system'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 5,
-          band: 'Average',
-          importance: null
-        }
-      },
-      {
-        key: 'bird-data-type',
-        answers: [
-          {
-            key: 'bird-data-type',
-            title:
-              'What poultry management data will you automatically collect and store?',
-            input: [
-              {
-                key: 'bird-data-type-A1',
-                value: 'Bird location'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 8,
-          band: 'Average',
-          importance: null
-        }
-      },
-      {
-        key: 'environmental-data-type',
-        answers: [
-          {
-            key: 'environmental-data-type',
-            title:
-              'What additional environmental data will you automatically collect and store?',
-            input: [
-              {
-                key: 'environmental-data-type-A1',
-                value: 'Ammonia'
-              }
-            ]
-          }
-        ],
-        rating: {
-          score: 8,
-          band: 'Average',
-          importance: null
-        }
+      ],
+      score: {
+        value: 24,
+        band: 'Strong'
       }
-    ],
-    overallRating: {
-      score: 26.73,
-      band: 'Weak'
+    },
+    {
+      questionId: '/adding-value',
+      category: 'Adding value',
+      fundingPriorities: [
+        'Improve processing and supply chains',
+        'Grow your business'
+      ],
+      answers: [
+        {
+          key: 'adding-value-A1',
+          value: 'Introducing a new product to your farm'
+        }
+      ],
+      score: {
+        value: 24.5,
+        band: 'Average'
+      }
+    },
+    {
+      questionId: '/project-impact',
+      category: 'Project impact',
+      fundingPriorities: [
+        'Improve processing and supply chains',
+        'Grow your business'
+      ],
+      answers: [
+        {
+          key: 'project-impact-A3',
+          value: 'Increasing range of added-value products'
+        }
+      ],
+      score: {
+        value: 4.5,
+        band: 'Average'
+      }
+    },
+    {
+      questionId: '/future-customers',
+      category: 'Future customers',
+      fundingPriorities: [
+        'Improve processing and supply chains',
+        'Grow your business'
+      ],
+      answers: [
+        {
+          key: 'future-customers-A1',
+          value: 'Processors'
+        }
+      ],
+      score: {
+        value: 0,
+        band: 'Weak'
+      }
+    },
+    {
+      questionId: '/collaboration',
+      category: 'Collaboration',
+      fundingPriorities: [
+        'Improve processing and supply chains',
+        'Encourage collaboration and partnerships'
+      ],
+      answers: [
+        {
+          key: 'collaboration-A1',
+          value: 'Yes'
+        }
+      ],
+      score: {
+        value: 10,
+        band: 'Strong'
+      }
+    },
+    {
+      questionId: '/environmental-impact',
+      category: 'Environmental Impact',
+      fundingPriorities: ['Improve the environment'],
+      answers: [
+        {
+          key: 'environmental-impact-A1',
+          value: 'Renewable energy'
+        },
+        {
+          key: 'environmental-impact-A2',
+          value: 'Energy efficiency'
+        },
+        {
+          key: 'environmental-impact-A3',
+          value: 'Water efficiency'
+        }
+      ],
+      score: {
+        value: 15.6,
+        band: 'Strong'
+      }
     }
-  }
+  ],
+  score: 16,
+  status: 'Eligible',
+  scoreBand: 'Strong'
 }
--- src/server/controllers/score-page.js
+++ src/server/controllers/score-page.js
@@ -1,8 +1,20 @@
 import scoreResponse from '~/src/server/controllers/mock-score-response.js'
+import { findPage } from '~/src/server/plugins/engine/helpers.js'
 import { QuestionPageController } from '~/src/server/plugins/engine/pageControllers/QuestionPageController.js'
 
 export default class ScorePageController extends QuestionPageController {
   viewName = 'grants/score-results'
+
+  getScoreChance(scoreBand) {
+    switch (scoreBand) {
+      case 'Strong':
+        return 'seems likely to'
+      case 'Average':
+        return 'might'
+      default:
+        return 'seems unlikely to'
+    }
+  }
 
   makeGetRouteHandler() {
     /**
@@ -14,10 +26,19 @@ export default class ScorePageController extends QuestionPageController {
     const fn = (request, context, h) => {
       const { collection, viewName } = this
       const viewModel = super.getViewModel(request, context)
-
       viewModel.errors = collection.getErrors(viewModel.errors)
-      viewModel.score = scoreResponse
+      viewModel.scoreChance = this.getScoreChance(scoreResponse.scoreBand)
+      viewModel.scoreBand = scoreResponse.scoreBand
 
+      const questions = scoreResponse.questions.map((question) => ({
+        category: question.category,
+        answers: question.answers,
+        scoreBand: question.score.band,
+        fundingPriorities: question.fundingPriorities,
+        title: findPage(this.model, question.questionId).title
+      }))
+
+      viewModel.questions = questions
       return h.view(viewName, viewModel)
     }
 
