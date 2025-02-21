79e9ecef5658dbab2e404256d509fcb04876e4a2
David Stone <davidjamesstone@gmail.com>
2025-02-21 18:03:20 +0000

    Update nunjucks liquid filter

--- src/server/plugins/nunjucks/filters/evaluate.js
+++ src/server/plugins/nunjucks/filters/evaluate.js
@@ -1,13 +1,15 @@
 import { evaluateTemplate } from '~/src/server/plugins/engine/helpers.js'
 
 /**
+ * Nunjucks filter to evaluate a liquid template.
+ * Current just used in `src/server/views/layout.html#LN37` for the pageTitle
  * @this {NunjucksContext}
  * @param {string} template
  */
 export function evaluate(template) {
   const { context } = this.ctx
 
-  if (!context) {
+  if (!context || typeof template !== 'string') {
     return template
   }
 
