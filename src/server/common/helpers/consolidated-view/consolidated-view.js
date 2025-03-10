3d9868152172e1ce8baaf2d6637cc460aca533b3
David Capilla <david.capilla@gmail.com>
2025-03-10 14:05:53 +0000

    RPS-435 - Ability to fetch data from CV refreshing a token

---
+++ src/server/common/helpers/consolidated-view/consolidated-view.js
@@ -0,0 +1,64 @@
+import { config } from '~/src/config/index.js'
+import { getValidToken } from '~/src/server/utils/token-manager.js'
+
+const CV_API_ENDPOINT = config.get('consolidatedView.apiEndpoint')
+const CV_API_AUTH_EMAIL = config.get('consolidatedView.authEmail')
+
+/**
+ * @typedef {object} BusinessResponse
+ * @property {object} [data] - The response data object
+ * @property {object} [data.business] - Business information
+ * @property {string} [data.business.sbi] - Standard Business Identifier
+ * @property {string} [data.business.organisationId] - Organisation identifier
+ * @property {object} [data.business.customer] - Customer details
+ * @property {string} [data.business.customer.firstName] - Customer's first name
+ * @property {string} [data.business.customer.lastName] - Customer's last name
+ * @property {string} [data.business.customer.role] - Customer's role
+ */
+
+/**
+ * Fetches business details from Consolidated View
+ * @param {number} sbi - Standard Business Identifier
+ * @param {number} crn - Customer Reference Number
+ * @returns {Promise<BusinessResponse>} - Promise that resolves to the business details
+ * @throws {Error} - If the request fails
+ */
+export async function fetchBusinessDetails(sbi, crn) {
+  const query = `
+    query Business {
+        business(sbi: "${sbi}") {
+            sbi
+            organisationId
+            customer(crn: "${crn}") {
+                firstName
+                lastName
+                role
+            }
+        }
+    }`
+
+  const token = await getValidToken()
+
+  const response = await fetch(CV_API_ENDPOINT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      email: CV_API_AUTH_EMAIL
+    },
+    body: JSON.stringify({
+      query
+    })
+  })
+
+  if (!response.ok) {
+    /**
+     * @type {Error & {code?: number}}
+     */
+    const error = new Error(response.statusText)
+    error.code = response.status
+    throw error
+  }
+
+  return /** @type {Promise<BusinessResponse>} */ (response.json())
+}
