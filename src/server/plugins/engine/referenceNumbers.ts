16f271ff2090f1b259ac9e7e4895921e27712979
Alex Luckett <alexgluckett@gmail.com>
2025-03-14 15:25:52 +0000

    Doc correction

--- src/server/plugins/engine/referenceNumbers.ts
+++ src/server/plugins/engine/referenceNumbers.ts
@@ -1,17 +1,17 @@
 import { randomBytes } from 'node:crypto'
 
 /**
- * Generates a reference number in the format of `XXX-XXX-XXX`, or `PREFIX-XXX-XXX` if a prefix is provided.
+ * Generates a reference number in the format of `XXXX-XXXX-XXXX`, or `PREFIX-XXXX-XXXX` if a prefix is provided.
  * Provides no guarantee on uniqueness.
  */
 export function generateReferenceNumber(prefix?: string) {
-  const segmentSize = 4
+  const segmentLength = 4
   const segmentCount = prefix ? 2 : 3
   prefix = prefix ? `${prefix}-` : ''
 
   const segments = Array.from(
     { length: segmentCount },
-    () => randomBytes(segmentSize).toString('hex').slice(0, segmentSize) // 0-9a-f, might be good enough?
+    () => randomBytes(segmentLength).toString('hex').slice(0, segmentLength) // 0-9a-f, might be good enough?
   )
 
   return `${prefix}${segments.join('-')}`.toUpperCase()
